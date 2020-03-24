cfe91facf284db633b7c5a3a4ce48f85097d89b7
Joan Pedro <jpos.senai@gmail.com>
2020-03-24 11:42:26 -0300

    Adiciona Template Inicial

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <div id="app" class="">
+      <h1> Template para Projetos Vue </h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  components: {
+  }
+}
+</script>
